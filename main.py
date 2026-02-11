dde4256f5757945048657e62d7cc3eda24af39f9
Olga Dementieva <lelik-0824@mail.ru>
2026-02-11 12:53:48 +0300

    final changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
     print(get_mask_card_number(7812345666565634))
