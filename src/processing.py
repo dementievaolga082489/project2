20dc284c0cfe3dd319c5494a0c3e66569f4d8a47
Olga Dementieva <08289@mail.ru>
2026-02-17 17:08:23 +0300

    изменение названия переменных.

--- src/processing.py
+++ src/processing.py
@@ -1,21 +1,21 @@
 from typing import Any
 
 
-def filter_by_state(list_dict: list[dict[str, Any]], state: str = 'EXECUTED') -> list[dict[str, Any]]:
+def filter_by_state(transactions: list[dict[str, Any]], state: str = 'EXECUTED') -> list[dict[str, Any]]:
     """Фильтрует список словарей по значению ключа 'state'."""
-    new_dict = []
-    for i in list_dict:
-        if i.get('state') == state:
-            new_dict.append(i)
-    return new_dict
+    result = []
+    for char in transactions:
+        if char.get('state') == state:
+            result.append(char)
+    return result
 
 
-def sort_by_date(list_dict: list[dict], descending: bool = True) -> list[dict]:
+def sort_by_date(records: list[dict], descending: bool = True) -> list[dict]:
     """
     Принимает список словарей и необязательный параметр, задающий порядок
     сортировки (по умолчанию — убывание). Функция должна возвращать новый список,
      отсортированный по дате.
      """
 
-    sorted_list = sorted(list_dict, key=lambda x: x['date'], reverse=descending)
+    sorted_list = sorted(records, key=lambda x: x['date'], reverse=descending)
     return sorted_list
