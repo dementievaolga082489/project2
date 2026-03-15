ff18c282ca45059977f08180ee121dd544d2a8cc
Olga Dementieva <08289@mail.ru>
2026-03-15 18:16:41 +0300

    Добавила  модуль external_api.py

---
+++ src/external_api.py
@@ -0,0 +1,48 @@
+import os
+from typing import Dict
+
+import requests
+from dotenv import load_dotenv
+
+# Загружаем переменную окружения
+load_dotenv(".env")
+
+API_KEY = os.getenv("API_KEY")
+
+
+def get_convert(transaction: Dict) -> float:
+    """
+      Принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, тип данных — float.
+     Если транзакция была в USD или EUR, происходит обращение к внешнему API для получения текущего курса валют
+    и конвертации суммы операции в рубли
+    """
+    # 1. Извлекаем данные из словаря
+    amount = float(transaction["operationAmount"]["amount"])
+    currency = transaction["operationAmount"]["currency"]["code"]
+
+    # 2. Если рубли — возвращаем как есть
+    if currency == "RUB":
+        return amount
+
+    # 3. Если USD или EUR — конвертируем
+    if currency in ["USD", "EUR"]:
+        to = "RUB"
+        from_ = currency
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from_}&amount={amount}"
+        headers = {"apikey": API_KEY}
+
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            result = response.json()
+            return float(result["result"])
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+            return 0.0
+        except requests.exceptions.RequestException as err:
+            print(f"Other error occurred: {err}")
+            return 0.0
+        except KeyError:
+            print("Unexpected response format")
+            return 0.0
+    return 0.0
